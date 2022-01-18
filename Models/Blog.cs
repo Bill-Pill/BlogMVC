ac5088c566c3da8d7b99152a714ab957d6c93d63
Bill-Pill <delashmuttwa@gmail.com>
2022-01-18 10:39:34 -0500

    Add models with navigational properties. Add enums.

---
+++ Models/Blog.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogMVC.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string AuthorId { get; set; } = string.Empty;
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
+        public string Description { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        [Display(Name = "Blog Image")]
+        public byte[] ImageData { get; set; } = Array.Empty<byte>();
+        [Display(Name = "Image Type")]
+        public string ContentType { get; set; } = string.Empty;
+        [NotMapped]
+        public IFormFile? Image { get; set; }
+
+        // Navigation Props
+        public virtual IdentityUser? Author { get; set; }
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+    }
+}
---
+++ Models/Tag.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogMVC.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; } = string.Empty;
+
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+        public string Text { get; set; } = string.Empty;
+
+        // Navigation props
+        public virtual Post? Post { get; set; }
+        public virtual IdentityUser? Author { get; set; }
+    }
+}
