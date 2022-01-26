d4248a50ee80161c6200a550819e082602efffde
Bill-Pill <delashmuttwa@gmail.com>
2022-01-26 12:46:04 -0500

    Default BlogUserId to null value for testing.  Allow nullables in ApplicationDbContext so green squigglies go away.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,10 +11,10 @@ namespace BlogMVC.Data
         {
         }
 
-        public DbSet<Blog> Blogs { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Blog>? Blogs { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<Comment>? Comments { get; set; }
+        public DbSet<Tag>? Tags { get; set; }
 
 
     }
