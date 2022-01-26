1ddac4ab153be17ff04d6d21789592d050811133
Bill-Pill <delashmuttwa@gmail.com>
2022-01-26 12:46:04 -0500

    Default BlogUserId to null value for testing.  Allow nullables in ApplicationDbContext so green squigglies go away.

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -7,7 +7,7 @@ namespace BlogMVC.Models
     public class Blog
     {
         public int Id { get; set; }
-        public string? BlogUserId { get; set; } = string.Empty;
+        public string? BlogUserId { get; set; } 
 
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
         public string Name { get; set; } = string.Empty;
