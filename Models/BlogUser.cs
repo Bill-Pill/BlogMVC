5e3d7c200a6c2df0d4c1c91041c139b7809c6123
Bill-Pill <delashmuttwa@gmail.com>
2022-01-31 11:26:27 -0500

    BlogUser Model changes - default certain values to NULL.

--- Models/BlogUser.cs
+++ Models/BlogUser.cs
@@ -8,18 +8,18 @@ namespace BlogMVC.Models
     {   
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
         [Display(Name = "First Name")]
-        public string FirstName { get; set; } = string.Empty;
+        public string FirstName { get; set; }
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
         [Display(Name = "Last Name")]
-        public string LastName { get; set; } = string.Empty;
+        public string LastName { get; set; }
 
         public byte[] ImageData { get; set; } = Array.Empty<byte>();
-        public string? ContentType { get; set; } = string.Empty;
+        public string? ContentType { get; set; }
 
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
-        public string? FacebookUrl { get; set; } = string.Empty;
+        public string? FacebookUrl { get; set; }
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
-        public string? TwitterUrl { get; set; } = string.Empty;
+        public string? TwitterUrl { get; set; }
 
         [NotMapped]
         public string FullName
