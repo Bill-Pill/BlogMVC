2894925d3c6693845254cfcde2109a478bdf2e3f
Bill-Pill <delashmuttwa@gmail.com>
2022-02-03 15:54:47 -0500

    Start implementing Email Service.

---
+++ ViewModels/MailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogMVC.ViewModels
+{
+    public class MailSettings
+    {
+        // To configure and use an SMTP server i.e. from Google
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
