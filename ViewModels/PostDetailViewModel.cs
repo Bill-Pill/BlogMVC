af0cf05cb16e168d16056097e966fd805ee0886d
Bill-Pill <delashmuttwa@gmail.com>
2022-02-08 17:28:57 -0500

    Add PostDetailViewModel and stylize Post Detail View.

---
+++ ViewModels/PostDetailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BlogMVC.Models;
+
+namespace BlogMVC.ViewModels
+{
+    public class PostDetailViewModel
+    {
+        public Post Post { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
+    }
+}
